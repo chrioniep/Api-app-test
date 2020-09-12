fc4bdd6ebca45ce726fe90dc781786d63733d99c
chrioniep <eponde26@gmail.com>
2020-09-12 01:34:24 +0100

    fix some bug

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 
 
-mongoose.connect(process.env.MONGODB_URL || "mongodb://127.0.0.1:27017/api-app-db", {
+mongoose.connect(process.env.MONGODB_URL, {
     useUnifiedTopology:true,
     useNewUrlParser:true,
     useCreateIndex:true,
